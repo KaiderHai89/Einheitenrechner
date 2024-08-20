946b968a42ca4e0ca65f714efe33416f6141bffb
KaiderHai <kai.meiners98@gmail.com>
2024-08-20 10:29:06 +0200

    V1.1
    Ausgabe für Umrechnung eingefügt

--- Einheitenrechner.py
+++ Einheitenrechner.py
@@ -1,5 +1,18 @@
+# Einheiten definieren
 skrupel = float(input("Wie viele Skrupel hast du? "))
 knoten = float(input("Mit wie viel Knoten fährst du? "))
 lachter = float(input("Wie viele Lachter hast du? "))
 gert = float(input("Wie viele Gerts hast du? "))
 koenigselle = float(input("Wie viele Königsellen hast du? "))
+# Umrechnungen definieren
+umrskr = skrupel * 1.25
+umrkno = knoten * 1.852
+umrlac = lachter * 1.938
+umrger = gert * 3.6
+umrkoe = koenigselle * 0.524
+# Ergebnisse ausgeben
+print(skrupel, "Skrupel sind", umrskr, "Gramm")
+print(knoten, "Knoten sind", umrkno, "Kilometer pro Stunde")
+print(lachter, "Lachter sind", umrlac, "Meter")
+print(gert, "Gert sind", umrger, "Meter")
+print(koenigselle, "Königsellen sind", umrkoe, "Meter")
